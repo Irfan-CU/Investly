03d908eafebcdb4b1be537ff703de8dcddcdf864
IrfanMustafa <irfanmustafa31201@gmail.com>
2020-10-22 23:44:09 -0400

    User Service and Dependecy Injection

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
+import {UserService} from './user.service';
 
 
 
@@ -36,7 +37,7 @@ import { HeaderComponent } from './header/header.component';
     MatToolbarModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
